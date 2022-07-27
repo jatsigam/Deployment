e6bb96358f2863687a94d065af1f30b684eb0b8d
Jatou Gaye <jatougaye@gmail.com>
2022-07-27 17:06:10 -0400

    fourth

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 const app = express()
 
-app.use('/js', express.static(path.join(__dirname, '/../public')))
+app.use(express.static(path.join(__dirname, '/../public')))
 
   app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4005
 
 app.listen(port, () => {console.log(`Listening on port ${port}`)})
 
