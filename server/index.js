8ed94e194deaec1712ee723436cf3a0bc665b311
Jatou Gaye <jatougaye@gmail.com>
2022-07-27 16:48:53 -0400

    issue fixed

--- server/index.js
+++ server/index.js
@@ -4,12 +4,15 @@ const path = require('path');
 
 const app = express()
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '../index.html'))
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.js'))
+  })
+
+  app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 const port = process.env.PORT || 4005
 
+app.listen(port, () => {console.log(`Listening on port ${port}`)})
 
-
-app.use('/js', express.static(path.join(__dirname, 'server/index.js')))
